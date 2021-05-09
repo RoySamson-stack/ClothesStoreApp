fa751a68fd7587a2b807cf163544031b90892ace
black-67 <reretsams@gmail.com>
2021-05-09 11:25:26 +0300

    practice app

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <View style={styles.carContainer}>
+        <ImageBackground source={require('./assets/img/ModelX.jpeg')}/>
+      <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subTitle}>Starting at $69, 420</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +25,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: {
+    width: '100%' ,
+    height: '100%',
+   
+  },
+  titles: {
+    marginTop: '30%',
+    width: "100%",
+    alignItems: 'center',
+  },
+  title:{
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    fontSize: 16,
+    color: 'gray',
+  }
 });
